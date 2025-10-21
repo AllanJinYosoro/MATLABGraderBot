cbe3a899b9500134d0d4e17e0d8185f8ef29c432
AllanJin <yosozura@163.com>
2025-10-21 13:08:24 +0800

    chore: update main

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from util.process_raw import process_raw
 from llm.Agent import Agent
 from util.grade_sequence import grade_sequence
 
-#QWEN official model: Agent(model_name='qwen-flash', base_url='https://dashscope.aliyuncs.com/compatible-mode/v1')
+#QWEN official model: Agent(model_name='qwen-flash', base_url='https://dashscope.aliyuncs.com/compatible-mode/v1', rate_limit=rate_limit)
+# POE Agent(model_name='Qwen3-235B-2507-FW', base_url='https://api.poe.com/v1', rate_limit=rate_limit)
 
 process_raw()
 rate_limit = AsyncLimiter(500, 60) #POE API Requests are rate-limited to 500 requests per minute per user
 
-grader = Agent(model_name='Qwen3-235B-2507-FW', base_url='https://api.poe.com/v1', rate_limit=rate_limit)
+grader = Agent(model_name='qwen-flash', base_url='https://dashscope.aliyuncs.com/compatible-mode/v1', rate_limit=rate_limit)
 asyncio.run(grade_sequence(grader=grader))
